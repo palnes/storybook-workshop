419f8f89165e9a2e1c122454903da12fa2f0a049
Isabella Kristiansen <iskr@fotoware.com>
2022-10-14 10:06:43 +0200

    stories fixes from worksop

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './Card.module.css';
 
 export function CardMedia({ imageURL }) {
-  // return <img src="http://placekitten.com/650/250" />;
   return <img src={imageURL} />;
 }
 
@@ -20,41 +19,13 @@ export function CardMain({ children }) {
 }
 
 export function CardFooter({ children }) {
-  return (
-    <footer className={styles.cardFooter}>
-      {children}
-    </footer>
-  );
+  return <footer className={styles.cardFooter}>{children}</footer>;
 }
 
 export function Card({ children, ...props }) {
   return (
     <div {...props} className={styles.card}>
-      <CardMedia imageURL="http://placekitten.com/650/249" />
-      <CardHeader title="Dark And Stormy" subTitle="Darkier and stormier!" />
-      <CardMain>
-        <p>
-          There once was 8 people in a room. It was a gloomy and wet day
-          outside. Wet enough to fill the water reserves in Oslo. After a long
-          day <a href="#">Egill</a> decided to open a window to let some fresh air in. Little
-          did he know that the wind was picking up and as Olivia went to close
-          it, the wind picked her up and through her out of the window! Thank
-          goodness Yesi was quick to her feet and ran down to catch her just in
-          time to prevent Olivias face to hit the pavement.
-        </p>
-        <p>
-          Sounds come from above. Sudden streams of baked good were now flying
-          out of the window. Was it the wind? Minutes later Isabella showed up
-          in the window, laughing and pointing as the croissant hit the ground.
-          Luckily Pål knew what to do and picked Vando up by his feet and used
-          him as a fishing nett to catch the baked good while still in the air.
-          Saving the workshop from hunger and anger.
-        </p>
-      </CardMain>
-      <CardFooter>
-        <button className={styles.button}>CLICK ME!</button>
-        <button className={styles.button}>CLICK ME TWICE!</button>
-      </CardFooter>
+      {children}
     </div>
   );
 }
--- src/Card/Card.stories.jsx
+++ src/Card/Card.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card } from './Card';
+import { Card, CardMedia, CardHeader, CardMain, CardFooter } from './Card';
+import styles from './Card.module.css';
 
 export default {
   title: 'Components/Card',
@@ -10,11 +11,91 @@ export default {
 const Template = (args) => <Card {...args} />;
 
 //👇 Each story then reuses that template
+export const Full = Template.bind({});
+Full.args = {
+  children: [
+    <>
+      <CardMedia imageURL="http://placekitten.com/650/249" />
+      <CardHeader title="Dark And Stormy" subTitle="Darkier and stormier!" />
+      <CardMain>
+        <p>
+          There once was 8 people in a room. It was a gloomy and wet day
+          outside. Wet enough to fill the water reserves in Oslo. After a long
+          day <a href="#">Egill</a> decided to open a window to let some fresh
+          air in. Little did he know that the wind was picking up and as Olivia
+          went to close it, the wind picked her up and through her out of the
+          window! Thank goodness Yesi was quick to her feet and ran down to
+          catch her just in time to prevent Olivias face to hit the pavement.
+        </p>
+        <p>
+          Sounds come from above. Sudden streams of baked good were now flying
+          out of the window. Was it the wind? Minutes later Isabella showed up
+          in the window, laughing and pointing as the croissant hit the ground.
+          Luckily Pål knew what to do and picked Vando up by his feet and used
+          him as a fishing nett to catch the baked good while still in the air.
+          Saving the workshop from hunger and anger.
+        </p>
+      </CardMain>
+      <CardFooter>
+        <button className={styles.button}>CLICK ME!</button>
+        <button className={styles.button}>CLICK ME TWICE!</button>
+      </CardFooter>
+    </>,
+  ],
+};
+
 export const Simple = Template.bind({});
 Simple.args = {
-  children: (
+  children: [
     <>
-      Bacon ipsum dolor amet chislic venison filet mignon tenderloin beef ribs andouille chicken buffalo sausage ham hock alcatra hamburger kielbasa. Meatloaf rump frankfurter, swine jowl andouille hamburger turducken meatball. Leberkas turducken salami shank, pork loin venison tongue strip steak pork belly cow biltong shankle. Tenderloin pork loin chuck, kevin jowl turducken doner shankle rump t-bone spare ribs porchetta short loin buffalo. Salami kevin shoulder andouille burgdoggen pork. Shoulder drumstick doner, rump spare ribs pork belly kevin porchetta tenderloin. Kielbasa capicola corned beef picanha bresaola pork chislic.
-    </>
-  ),
+      <CardHeader title="Dark And Stormy" subTitle="Darkier and stormier!" />
+      <CardMain>
+        <p>
+          There once was 8 people in a room. It was a gloomy and wet day
+          outside. Wet enough to fill the water reserves in Oslo. After a long
+          day <a href="#">Egill</a> decided to open a window to let some fresh
+          air in. Little did he know that the wind was picking up and as Olivia
+          went to close it, the wind picked her up and through her out of the
+          window! Thank goodness Yesi was quick to her feet and ran down to
+          catch her just in time to prevent Olivias face to hit the pavement.
+        </p>
+        <p>
+          Sounds come from above. Sudden streams of baked good were now flying
+          out of the window. Was it the wind? Minutes later Isabella showed up
+          in the window, laughing and pointing as the croissant hit the ground.
+          Luckily Pål knew what to do and picked Vando up by his feet and used
+          him as a fishing nett to catch the baked good while still in the air.
+          Saving the workshop from hunger and anger.
+        </p>
+      </CardMain>
+    </>,
+  ],
+};
+
+export const ImageCaption = Template.bind({});
+ImageCaption.args = {
+  children: [
+    <>
+      <CardMedia imageURL="http://placekitten.com/650/249" />
+      <CardMain>
+        <p>
+          There once was 8 people in a room. It was a gloomy and wet day
+          outside. Wet enough to fill the water reserves in Oslo. After a long
+          day <a href="#">Egill</a> decided to open a window to let some fresh
+          air in. Little did he know that the wind was picking up and as Olivia
+          went to close it, the wind picked her up and through her out of the
+          window! Thank goodness Yesi was quick to her feet and ran down to
+          catch her just in time to prevent Olivias face to hit the pavement.
+        </p>
+        <p>
+          Sounds come from above. Sudden streams of baked good were now flying
+          out of the window. Was it the wind? Minutes later Isabella showed up
+          in the window, laughing and pointing as the croissant hit the ground.
+          Luckily Pål knew what to do and picked Vando up by his feet and used
+          him as a fishing nett to catch the baked good while still in the air.
+          Saving the workshop from hunger and anger.
+        </p>
+      </CardMain>
+    </>,
+  ],
 };
